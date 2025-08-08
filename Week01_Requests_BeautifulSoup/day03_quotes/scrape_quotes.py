b000ab065f1e9ce1e6a8267eb235980ad425edc0
M.Maroof <maroof96965@gmail.com>
2025-08-08 10:39:20 +0500

    Day3

--- Week01_Requests_BeautifulSoup/day03_quotes/scrape_quotes.py
+++ Week01_Requests_BeautifulSoup/day03_quotes/scrape_quotes.py
@@ -1,2 +1,25 @@
-# scrape_quotes.py
+import requests
+from bs4 import BeautifulSoup
+import json 
 
+url = "https://quotes.toscrape.com"
+response = requests.get(url)
+soup = BeautifulSoup(response.text,"html.parser")
+
+quotes_data=[]
+
+for qoute in soup.find_all("div",class_="quote"):
+    text = qoute.find("span",class_="text").text.strip()
+    author = qoute.find("small",class_="author").text.strip()
+    tags = [tag.text for tag in qoute.find_all("a",class_="tag")]
+
+    quotes_data.append({
+        "text":text,
+        "author":author,
+        "tags":tags
+    })
+
+with open("output_quotes.json","w",encoding="utf-8") as f:
+    json.dump(quotes_data,f,indent=4,ensure_ascii=False)
+
+print(f"✅ Scraped {len(quotes_data)} quotes and saved to output_quotes.json")
